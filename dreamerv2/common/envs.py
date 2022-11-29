e52c5e79e73a81bc1ae7af8633523962f36a4815
s183193 <63309900+s183193@users.noreply.github.com>
2022-11-29 17:22:49 +0100

    t

--- dreamerv2/common/envs.py
+++ dreamerv2/common/envs.py
@@ -75,7 +75,6 @@ class DMC:
   def __init__(self, name, action_repeat=1, size=(64, 64), camera=None):
     os.environ['MUJOCO_GL'] = 'egl'
     print(name)
-    sys.exit()
     domain, task = name.split('_', 1)
     if domain == 'cup':  # Only domain with multiple words.
       domain = 'ball_in_cup'
