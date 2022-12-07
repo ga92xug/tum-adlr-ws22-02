800677a68bcdc642061a9dca68c08c9fd502ecf5
s183193 <63309900+s183193@users.noreply.github.com>
2022-12-07 10:55:59 +0100

    vis2

--- dreamerv2/agent.py
+++ dreamerv2/agent.py
@@ -94,6 +94,8 @@ class WorldModel(common.Module):
     for name in config.grad_heads:
       assert name in self.heads, name
     self.model_opt = common.Optimizer('model', **config.model_opt)
+    print("Model_Opt: \n")
+    print(self.model_opt)
 
   def train(self, data, state=None):
     with tf.GradientTape() as model_tape:
