46d64358ba1d5f77f0d7a4414537b53fdfd0e206
ga92xug <stefan.m.frisch@gmail.com>
2023-01-07 16:50:07 +0100

    config set for linear fad in start

--- dreamerv2/train.py
+++ dreamerv2/train.py
@@ -201,12 +201,15 @@ def main():
   train_driver.on_step(train_step)
 
   while step < config.steps:
-    if step >= 10000:
+    if step >= config.start_external_reward:
       # linear fade-in from grab to stacking reward
       config = config.update({
           'reward_weight': 1.0,
-          'grab_reward_weight': (1.0 - step.value / config.steps),
-          'stacking_reward_weight': (0.0 + step.value / config.steps)})
+          'grab_reward_weight': (1.0 - (step.value - config.start_external_reward)\
+                                / (config.steps - config.start_external_reward)),
+          'stacking_reward_weight': (0.0 + (step.value - config.start_external_reward)\
+                                / (config.steps - config.start_external_reward))
+      })
 
     logger.write()
     print('Start evaluation.')
