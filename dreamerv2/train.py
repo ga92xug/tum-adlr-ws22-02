5283bb8df4d34e1332886b8d14e464ccdf3e9ccd
s183193 <63309900+s183193@users.noreply.github.com>
2023-01-10 20:54:29 +0100

    reward weight adjustment

--- dreamerv2/train.py
+++ dreamerv2/train.py
@@ -204,7 +204,7 @@ def main():
     if step >= config.start_external_reward:
       # linear fade-in from grab to stacking reward
       config = config.update({
-          'reward_weight': 1.0,
+          'reward_weight': 0.0,
           'grab_reward_weight': (1.0 - (step.value - config.start_external_reward)\
                                 / (config.steps - config.start_external_reward)),
           'stacking_reward_weight': (0.0 + (step.value - config.start_external_reward)\
