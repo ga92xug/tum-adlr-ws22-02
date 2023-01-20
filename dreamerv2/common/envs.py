02a75635d3c11ffd3d6a49c94d32369f995899df
s183193 <63309900+s183193@users.noreply.github.com>
2023-01-20 18:39:46 +0100

    stacking reward optimized 2

--- dreamerv2/common/envs.py
+++ dreamerv2/common/envs.py
@@ -372,7 +372,7 @@ class DMC:
         # (values show wall center at x-pos)
         #print("Prev: "+str(prev_box_pos_z))
         #print(box_pos_z)
-        
+        '''
         # Stacking reward version 1: 
         if (box_pos_z[i] > 0.065) and (box_pos_z[i]<0.18) and (box_pos_x[i]>(-0.682843+0.3)) and (box_pos_x[i]<(0.682843-0.3)): # total box height ca. 0.044 -> ca. 0.066 for box stacked on other box
             if box_pos_z[i] > (prev_box_pos_z[i] + box_height_threshold):
@@ -385,13 +385,16 @@ class DMC:
                 
         '''
         # Stacking reward version 2:
-        if box_pos_z[i] > 0.065:
+        if (box_pos_z[i] > 0.065) and box_pos_z[i]<0.18:
             for j in range(n_boxes):
-                if box_pos_z[i] > box_pos_z[j] + 0.042: #ca. one box height
-                    if sim.site_distance('box'+str(j), 'box'+str(i)) < 0.01:
+                if box_pos_z[i] > box_pos_z[j] + 0.04: #ca. one box height
+                    if sim.site_distance('box'+str(j), 'box'+str(i)) < 0.05:
                         if (box_pos_x[i]>(-0.682843+0.3)) and (box_pos_x[i]<(0.682843-0.3)):
                             reward += 1
-        '''
+        print(box_pos_z)
+        print(reward)
+        time.sleep(1)
+        
         
     return reward, box_pos, box_pos_z
 
